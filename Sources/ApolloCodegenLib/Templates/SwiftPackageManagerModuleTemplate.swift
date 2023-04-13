c4ba36619b13805e80f92826d2dcc7bf502d7071
Yonas Kolb <yonaskolb@users.noreply.github.com>
2023-04-13 10:15:29 +1000

    update version in package template

--- Sources/ApolloCodegenLib/Templates/SwiftPackageManagerModuleTemplate.swift
+++ Sources/ApolloCodegenLib/Templates/SwiftPackageManagerModuleTemplate.swift
@@ -35,7 +35,7 @@ struct SwiftPackageManagerModuleTemplate: TemplateRenderer {
         """})
       ],
       dependencies: [
-        .package(url: "https://github.com/figs-engineering/apollo-ios.git", exact: "1.1.0-mutable"),
+        .package(url: "https://github.com/figs-engineering/apollo-ios.git", exact: "1.1.2-mutable"),
       ],
       targets: [
         .target(
